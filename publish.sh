717a0b2699e12f2a9c0c132bff458b3e3ae41143
lalyos <lalyos@yahoo.com>
2017-10-08 15:45:55 +0200

    fix publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 git submodule update --init --recursive
+rm -rf public/
+git worktree add -B master public origin/master
 hugo && cd public && git add --all && git commit -m Publishing ; cd .. ; git push origin master
